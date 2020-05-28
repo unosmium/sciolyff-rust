ea467d7e32266d495f69014fb136c245bfc368bd
Em Zhan <zqianem@gmail.com>
2020-05-28 18:55:53 -0400

    Implement focusing of event results
    (except for sorting by order)

--- src/interpreter/html/script.js
+++ src/interpreter/html/script.js
@@ -4,8 +4,8 @@ const rows = [...document.querySelectorAll('tbody tr')];
 const enCollator = new Intl.Collator('en');
 
 function compareTeamRank(rowA, rowB) {
-  let rankA = parseInt(rowA.querySelector('td:nth-child(3)').textContent);
-  let rankB = parseInt(rowB.querySelector('td:nth-child(3)').textContent);
+  let rankA = parseInt(rowA.querySelector('td:nth-child(4)').textContent);
+  let rankB = parseInt(rowB.querySelector('td:nth-child(4)').textContent);
   return rankA - rankB;
 }
 
@@ -46,3 +46,31 @@ function sortTable(option) {
 }
 
 sortSelect.addEventListener('change', (e) => sortTable(e.target.value));
+
+///////////////////////////////////////////////////////////////////////////////
+
+const focusSelect = document.querySelector('#focus');
+const focusHeader = document.querySelector('th:nth-child(3)');
+const focusColumn = [...document.querySelectorAll('td:nth-child(3)')];
+
+focusSelect.addEventListener('change', (e) => {
+  let col = parseInt(e.target.value);
+
+  if (col === 0) {
+    focusHeader.style.cssText = '';
+    focusHeader.style.width = '';
+    focusHeader.innerHTML = '';
+    focusColumn.forEach((td, index) => td.innerHTML = '');
+    return;
+  }
+
+  focusHeader.style.cssText = 'width:4em;direction:rtl;padding-right:0.5em;';
+
+  let eventHeader = document.querySelector(`th:nth-child(${col + 5})`);
+  let eventColumn = [...document.querySelectorAll(`td:nth-child(${col + 5})`)];
+
+  focusHeader.innerHTML = eventHeader.innerHTML;
+  focusColumn.forEach((td, index) => {
+    td.innerHTML = eventColumn[index].innerHTML;
+  });
+});
